d2dc118b8a12f2809e71598e8ee965b6f8685338
hisoka777 <56453432+hisoka777@users.noreply.github.com>
2021-10-25 12:17:45 +0200

    primera version

--- EverisGit1AMAyman2/src/main/java/com/everis/EverisGit1AMAyman2/App.java
+++ EverisGit1AMAyman2/src/main/java/com/everis/EverisGit1AMAyman2/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Ayman Alaoui !" );
+        System.out.println( "Hola mundo !" );
     }
 }
